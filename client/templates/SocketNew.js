48f1944656ec6db89dacc5075b6b7269382dfa6c
AtecStudios <Accounts@atecstudios.com>
2016-03-23 02:52:41 -0700

    Some Validation

--- client/templates/SocketNew.js
+++ client/templates/SocketNew.js
@@ -1,18 +1,45 @@
+var GoodToGo = true;
+
 Template.SocketNew.events({
+    "input #name": function(event){
+      event.preventDefault();
+      if($("#name").hasClass('invalid')){
+        GoodToGo = false;
+      }
+      if($("#name").hasClass('valid')){
+        GoodToGo = true;
+      }
+    },
     "submit .new-socket": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
-      // Get value from form element
-      var Socket=  {
-        owner: Session.get("ClientID"),
-        user: false,
-        name: event.target.name.value,
-        port: event.target.port.value,
-        type: event.target.socketType.value,
+      var name = event.target.name.value;
+      var port = event.target.port.value;
+      var type = event.target.socketType.value;
+      if(name == ""){
+        GoodToGo = false;
+        alert("Plese Enter A Name!");
+        return;
       }
-      Meteor.call("newSocket", Socket);
-      // Clear form
-      event.target.name.value = "";
+      if(type == ""){
+          GoodToGo = false;
+          alert("Plese Enter A Type!");
+          return;
+      }
+
+      // Get value from form element
+      if(true){
+        var Socket=  {
+          owner: Session.get("ClientID"),
+          user: false,
+          name: name,
+          port: port,
+          type: type,
+        }
+        Meteor.call("newSocket", Socket);
+        // Clear form
+        event.target.name.value = "";
+      }
     }
   });
